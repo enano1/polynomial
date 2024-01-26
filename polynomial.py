56f62cddf5fcaad5528d0cc53466a76a9c5b526b
Paul Enano <paul.m.enano@gmail.com>
2024-01-26 00:44:12 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, x):
+        return x
 
 class Int:
     def __init__(self, i):
@@ -11,6 +14,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -25,6 +31,9 @@ class Add:
         if isinstance(self.p2, Add):
             return repr(self.p1) + " + ( " + repr(self.p2) + " )"
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
 class Sub:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -39,6 +48,8 @@ class Sub:
             return repr(self.p1) + " - ( " + repr(self.p2) + " )"
         return repr(self.p1) + " - " + repr(self.p2)
     
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -53,6 +64,8 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
 class Div:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -67,6 +80,9 @@ class Div:
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
     
+    def evaluate(self, x):
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
+    
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
 poly2 = Add(Mul(X(), Div(X(), Int(2))), Sub(Int(5), X()))
@@ -75,4 +91,8 @@ poly3 = Div(Add(Int(1), Int(2)), Add(Int(3), Int(4)))
 print(poly3)
 poly4 = Add(Mul(X(), X()), Sub(X(), Int(1)))
 print(poly4)
+poly5 = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
+print(poly5.evaluate(-1))
+poly6 = Add(Mul(X(), X()), Sub(X(), Int(1)))
+print(poly6.evaluate(-1))
 
